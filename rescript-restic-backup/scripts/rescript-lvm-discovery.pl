167829e89ba24b9ac6a9ff31bb3b69d4e0f06381
Sebastian Plocek <sebastian@plocek.at>
2020-02-12 23:41:34 +0100

    Rewrote the script to send to zabix all lv-repo combinations at every runtime. This addresses an issue where zabbix deleted items that had not been mentioned at the most recent run.

--- rescript-restic-backup/scripts/rescript-lvm-discovery.pl
+++ rescript-restic-backup/scripts/rescript-lvm-discovery.pl
@@ -1,43 +1,52 @@
 #!/usr/bin/perl -w
 
-$first = 1;
-$repo = $ENV{'REPO'};
-$lv_to_backup = $ENV{'LV_TO_BACKUP'};
+# This script identifies Logical Volumes and
+# Rescript Repositories configured, to create
+# Zabbix Items for every possible combination
 
-# Check provided [lv_name] or list of [lv_names]
-if (-f $lv_to_backup) {
-	# If a file was provided.
-	# print "Path to a file was provided";
-	for (`grep -v '^#' $lv_to_backup`)
-	{
-		# Check each LV existence
-		$lvs = "";
-		$lvs = `lvs --noheading -o lv_name | grep -ow "$_"`;
-		# Remove NewLine from String
-		$_ =~ s/\R//g;
-		if ($lvs eq "") {print "LV $_ was not found."; exit 1;}
-		push @lv_names, $_;
-	}
-} else {
-	# If a string was provided.
-	# print "lv_name was provided";
-	# Check if LV exists.
-	$lvs = `lvs --noheading -o lv_name | grep -ow "$lv_to_backup"`;
-	if ($lvs eq "") {print "LV $lv_to_backup was not found."; exit 1;}
-	@lv_names = $lv_to_backup;
-}
+# Author: Sebastian Plocek
+# https://github.com/sebastian13/zabbix-templates
+# https://github.com/sebastian13/lvm-restic-backup
+
+use strict;
+use warnings;
+use 5.010;
+
+# Save Path to Rescript Config Files
+my $home = $ENV{"HOME"};
+my $rescript_dir = $home . "/.rescript";
+my $config_dir = "$rescript_dir/config";
+
+# Get the names of all Rescript Repositories
+opendir(DIR, $config_dir) || die "cannot open directory: $!";
+my @repos = grep { /\.conf$/ } readdir(DIR);
+closedir(DIR);
+# Remove .conf to get the repo name
+for (@repos) { s/.conf//; };
+
+# Get the names of all Logical Volumes
+# Exclude Snapshots and LV name containing swap or *swp
+my @lv_names = `lvs --noheading -o lv_name -S 'lv_attr !~ ^s' | grep -v -e 'swap' -e 'swp' | tr -d '  '`;
+# Delete Empty Lines
+chomp @lv_names;
+
+my $first = 1;
 
 print "{\"data\":[";
 
-for $_ (@lv_names)
+foreach my $repo (@repos) 
 {
-    print "," if !$first;
-    $first = 0;
+    foreach my $lv (@lv_names)
+    {
+    	print "," if !$first;
+    	$first = 0;
 
-    print "{";
-    print "\"{#REPO}\":\"$repo\",";
-    print "\"{#LV}\":\"$_\"";
-    print "}";
+    	print "{";
+    	print "\"{#REPO}\":\"$repo\"";
+    	print ",";
+    	print "\"{#LV}\":\"$lv\"";
+    	print "}";
+    }
 }
 
 print "]}";
